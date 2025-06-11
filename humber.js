f92c586c9696d8adf97b14128d9ee3ee0c46c11e
NagalakshmiJK <jklakshmi2066@gmail.com>
2025-06-11 11:59:42 +0530

    feat : adding new field in js

--- humber.js
+++ humber.js
@@ -1,36 +1,32 @@
  function showslidermenu(){
     const slidermenu = document.querySelector('.slidermenu')
-    slidermenu.style.display='flex'
-    const humber = document.querySelector('.humber img')
-    humber.style.opacity='0%'
+    slidermenu.style.display='block'
     const content = document.querySelector('.content')
     content.style.display='none'
     const logo2 = document.querySelector('.logo2')
     logo2.style.display='none'
-    const cancel = document.querySelector('.cancel')
-    cancel.style.left='84%';
-    cancel.style.position='absolute';
+    const humber = document.querySelector('.humber')
+    humber.style.display='none'
 }
 
 function hideslidermenu(){
-    // console.log("hhhi");
     const slidermenu = document.querySelector('.slidermenu')
     slidermenu.style.display='none'
-    const humber = document.querySelector('.humber img')
-    humber.style.opacity='100%'
     const content = document.querySelector('.content')
     content.style.display='block'
     const logo2 = document.querySelector('.logo2')
     logo2.style.display='flex'
+    const humber = document.querySelector('.humber')
+    humber.style.display='flex'
 }
 
 function lists(){
     const slidermenu = document.querySelector('.slidermenu')
     slidermenu.style.display='none'
-    const humber = document.querySelector('.humber img')
-    humber.style.opacity='100%'
     const content = document.querySelector('.content')
     content.style.display='block'
-        const logo2 = document.querySelector('.logo2')
+    const logo2 = document.querySelector('.logo2')
     logo2.style.display='flex'
+    const humber = document.querySelector('.humber')
+    humber.style.display='flex'
 }
